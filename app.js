5073840bc5731286c616c249919b5fbbeb877e5b
Kamila Nitta <kamila.nitta@gmail.com>
2021-07-13 12:46:17 +0200

    first commit

--- app.js
+++ app.js
@@ -7,6 +7,41 @@ const allPokemon = require("./data");
 
 const app = express();
 
+app.get("/pokemon", (request, response) => {
+  //   console.log("test get");
+  return response.json(allPokemon);
+});
+
+app.get("/pokemon/search", (request, response) => {
+  //   response.send(request.query);
+  const queryParams = response.query;
+  console.log(queryParams);
+
+  for (let key in queryParams) {
+    const foundPokemon = allPokemon.find((pokemon) => {
+      return pokemon[key]
+        .toLowerCase()
+        .includes(queryParams[key].toLowerCase());
+    });
+    if (foundPokemon) {
+      return response.json(foundPokemon);
+    } else {
+      return response.json({ msg: "Pokemon not found" });
+    }
+  }
+  response.json(queryParams);
+});
+
+app.get("/pokemon/:id", (request, response) => {
+  const id = request.params.id;
+
+  const foundPokemon = allPokemon.find((pokemon) => {
+    return pokemon.id === Number(id);
+  });
+  //   console.log(foundPokemon);
+  return response.json(foundPokemon);
+});
+
 // -- Define your route listeners here! --
 
 app.listen(PORT, () => console.log(`Server up and running at port ${PORT}`));
